23787c218a0eb7db0b0fbce0ea2d1793fea1b44a
AldairCruz501 <acruz.pbxhosting@gmail.com>
2025-01-27 11:34:23 -0600

    Sitio Web Juridico Nidom

---
+++ JURIDICONIDOM/web/enviar.php
@@ -0,0 +1,68 @@
+<?php
+if($_POST['g-recaptcha-response'] == null || !isset($_POST['g-recaptcha-response']) || $_POST['g-recaptcha-response'] == ''){
+    echo "<script>alert('Complete el reCAPTCHA para continuar')</script>";
+    echo "<script>window.location.replace('index.html#contacto');</script>";
+    return;
+}elseif(isset($_POST['correo']) && isset($_POST['nombre']) && isset($_POST['numero']) && isset($_POST['mensaje']) && isset($_POST['asunto'])){
+    $correo= trim($_POST['correo']);
+    $nombre = trim($_POST['nombre']);
+    $numero = trim($_POST['numero']);
+    $mensaje = trim($_POST['mensaje']);
+    $asunto = trim($_POST['asunto']);
+
+    $message =  "Nombre: " . $nombre . "\n Correo: " . $correo .  "\n Numero de contacto: " . $numero .  "\n Asunto: " . $asunto  .  "\n Mensaje: " . $mensaje;
+
+
+    $response = $_POST['g-recaptcha-response'];
+    $secret = '6LchCIQoAAAAADQwu3KoWGDWjKIExZBUN4n_ee_A';
+    $url = "https://www.google.com/recaptcha/api/siteverify";
+
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 
+        http_build_query(
+            array(
+                'secret'=>$secret,
+                'response'=>$response
+            )
+        )
+    );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    $result = json_decode($result);
+
+    if($result->success){
+        // $to = 'ana.barrera@jjbusinessconsulting.net';
+        $to = 'echong.pbxhosting@gmail.com';
+        $subject = 'Formulario de contacto';
+
+
+        $mail = mail($to, $subject, $message);
+        if($mail){
+            echo "<script>alert('Formulario enviado con exito')</script>";
+            echo "<script>window.location.replace('index.html');</script>";
+            return;
+        }else{
+            echo "<script>alert('Ocurrio un error al enviar el formulario')</script>";
+            echo "<script>window.location.replace('index.html');</script>";
+            return;
+        }
+    }
+
+}else{
+    echo "<script>alert('Llene todos los campos obligatorios del formulario para continuar')</script>";
+    echo "<script>window.location.replace('index.html#contacto');</script>";
+    return;
+}
+
+?>
+
+
+
+
+
+
