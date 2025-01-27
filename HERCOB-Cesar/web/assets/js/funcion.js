5d8413470f0c9cdc6ab068efd6dc2c56d1b603f2
AldairCruz501 <acruz.pbxhosting@gmail.com>
2025-01-27 11:19:29 -0600

    Sitio Web Hercob

---
+++ HERCOB-Cesar/web/assets/js/funcion.js
@@ -0,0 +1,103 @@
+function gestoria(){
+    document.getElementById('gestoria').style.display = 'block';
+    document.getElementById('ambulancia').style.display = "none";
+    document.getElementById('consultoria').style.display = 'none';
+    document.getElementById('ejecutiva').style.display = "none";
+    document.getElementById('carga').style.display = "none";
+    document.getElementById('courier').style.display = "none";
+}
+function consultoria(){
+    document.getElementById('consultoria').style.display = "block";
+    document.getElementById('gestoria').style.display = 'none';
+    document.getElementById('ambulancia').style.display = "none";
+    document.getElementById('ejecutiva').style.display = "none";
+    document.getElementById('carga').style.display = "none";
+    document.getElementById('courier').style.display = "none";
+}
+function ambulancia(){
+    document.getElementById('ambulancia').style.display = "block";
+    document.getElementById('consultoria').style.display = 'none';
+    document.getElementById('gestoria').style.display = 'none';
+    document.getElementById('ejecutiva').style.display = "none";
+    document.getElementById('carga').style.display = "none";
+    document.getElementById('courier').style.display = "none";
+}
+function ejecutiva(){
+    document.getElementById('ejecutiva').style.display = "block";
+    document.getElementById('consultoria').style.display = 'none';
+    document.getElementById('gestoria').style.display = 'none';
+    document.getElementById('ambulancia').style.display = "none";
+    document.getElementById('carga').style.display = "none";
+    document.getElementById('courier').style.display = "none";
+}
+function carga(){
+    document.getElementById('carga').style.display = "block";
+    document.getElementById('consultoria').style.display = 'none';
+    document.getElementById('gestoria').style.display = 'none';
+    document.getElementById('ambulancia').style.display = "none";
+    document.getElementById('ejecutiva').style.display = "none";
+    document.getElementById('courier').style.display = "none";
+}
+function courier(){
+    document.getElementById('courier').style.display = "block";
+    document.getElementById('consultoria').style.display = 'none';
+    document.getElementById('gestoria').style.display = 'none';
+    document.getElementById('ambulancia').style.display = "none";
+    document.getElementById('ejecutiva').style.display = "none";
+    document.getElementById('carga').style.display = "none";
+
+}
+
+
+/*SCRIPTS mo*/ 
+
+function gestoriamovil(){
+    document.getElementById('gestoria-mo').style.display = 'block';
+    document.getElementById('ambulancia-mo').style.display = "none";
+    document.getElementById('consultoria-mo').style.display = 'none';
+    document.getElementById('ejecutiva-mo').style.display = "none";
+    document.getElementById('carga-mo').style.display = "none";
+    document.getElementById('courier-mo').style.display = "none";
+}
+function consultoriamovil(){
+    document.getElementById('consultoria-mo').style.display = "block";
+    document.getElementById('gestoria-mo').style.display = 'none';
+    document.getElementById('ambulancia-mo').style.display = "none";
+    document.getElementById('ejecutiva-mo').style.display = "none";
+    document.getElementById('carga-mo').style.display = "none";
+    document.getElementById('courier-mo').style.display = "none";
+}
+function ambulanciamovil(){
+    document.getElementById('ambulancia-mo').style.display = "block";
+    document.getElementById('consultoria-mo').style.display = 'none';
+    document.getElementById('gestoria-mo').style.display = 'none';
+    document.getElementById('ejecutiva-mo').style.display = "none";
+    document.getElementById('carga-mo').style.display = "none";
+    document.getElementById('courier-mo').style.display = "none";
+}
+function ejecutivamovil(){
+    document.getElementById('ejecutiva-mo').style.display = "block";
+    document.getElementById('consultoria-mo').style.display = 'none';
+    document.getElementById('gestoria-mo').style.display = 'none';
+    document.getElementById('ambulancia-mo').style.display = "none";
+    document.getElementById('carga-mo').style.display = "none";
+    document.getElementById('courier-mo').style.display = "none";
+}
+function cargamovil(){
+    document.getElementById('carga-mo').style.display = "block";
+    document.getElementById('consultoria-mo').style.display = 'none';
+    document.getElementById('gestoria-mo').style.display = 'none';
+    document.getElementById('ambulancia-mo').style.display = "none";
+    document.getElementById('ejecutiva-mo').style.display = "none";
+    document.getElementById('courier-mo').style.display = "none";
+}
+function couriermovil(){
+    document.getElementById('courier-mo').style.display = "block";
+    document.getElementById('consultoria-mo').style.display = 'none';
+    document.getElementById('gestoria-mo').style.display = 'none';
+    document.getElementById('ambulancia-mo').style.display = "none";
+    document.getElementById('ejecutiva-mo').style.display = "none";
+    document.getElementById('carga-mo').style.display = "none";
+
+}
+
---
+++ HERCOB-Cesar/web/enviar.php
@@ -0,0 +1,56 @@
+<?php
+
+if ($_POST['g-recaptcha-response'] == '') {
+
+} else {
+$obj = new stdClass();
+$obj->secret = "6LcxnLwjAAAAALkRf-IX_oWzF2o0W3i9KeOM-las";
+$obj->response = $_POST['g-recaptcha-response'];
+$obj->remoteip = $_SERVER['REMOTE_ADDR'];
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+
+$options = array(
+'http' => array(
+'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+'method' => 'POST',
+'content' => http_build_query($obj)
+)
+);
+$context = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+$validar = json_decode($result);
+
+/*  FIN DE CAPTCHA   */
+
+if ($validar->success) {
+$correo= trim($_POST['correo']);
+$nombre = trim($_POST['nombre']);
+$celular = trim($_POST['celular']);
+$servicio = trim($_POST['servicio']);
+$mensaje = trim($_POSTY['mensaje']);
+
+
+$consulta =  "Nombre: " . $nombre . "\n Correo: " . $correo .  "\n Numero de contacto: " . $celular  .  "\n Servicio de interes: " . $servicio .  "\n Mensaje: " . $mensaje;
+          echo '
+                    <script>
+                         alert("Gracias por comunicarte");
+                         window.location = "https://hercob.com.mx/";
+                    </script>        
+               ';
+
+          mail("echong.pbxhosting@gmail.com", "Formulario Hercob", $consulta);header("location:index.html");
+} 
+
+     else {
+          echo '
+          <script>
+               alert("Error resuelva el captcha");
+               window.location = "https://hercob.com.mx/";
+          </script>        
+          ';
+          header("location:index.html");
+
+     }
+}
+?>
