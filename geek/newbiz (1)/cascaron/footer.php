6546d33b1baa81a432a1a6ea9e4308547ca8889c
AldairCruz501 <acruz.pbxhosting@gmail.com>
2025-01-27 11:14:04 -0600

    Sitio Web Geeky Tecnologias

---
+++ geek/newbiz (1)/cascaron/footer.php
@@ -0,0 +1,70 @@
+
+     <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-info">
+            <div class="logo-white">
+              <img src="assets/img/logonew-blanco.png" alt="">
+            </div>
+
+            <p>PbxHosting mejora la comunicación, la eficiencia y la productividad de las empresas, ofreciendo servicios de alta calidad, flexibilidad y rentabilidad en la transmisión de llamadas por Internet. </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Enlaces</h4>
+            <ul>
+              <li><a href="#">Inicio</a></li>
+              <li><a href="#">Nosotros</a></li>
+              <li><a href="#">Servicios</a></li>
+              <li><a href="#">Terminos y Condiciones</a></li>
+              <li><a href="#">Politica de Privacidad</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contacto</h4>
+            <p>
+              Horario de atención: <br>
+              <b>Lunes a Viernes</b> <br>
+              9:00am a 6:00pm <br>
+              <b>Sábados</b> <br>
+              9:00am a 2:00pm  <br>
+              <strong>Teléfono</strong> 55 4170 8990<br>
+              <strong>Correo:</strong> contacto@pbxhosting.com.mx
+            </p>
+              
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="fa-brands fa-twitter"></i></a>
+              <a href="#" class="facebook"><i class="fa-brands fa-facebook"></i></a>
+              <a href="#" class="instagram"><i class="fa-brands fa-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="fa-brands fa-tiktok"></i></a>
+              <a href="#" class="linkedin"><i class="fa-brands fa-linkedin"></i></a>
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-newsletter">
+            <h4>Suscribete a nuestro Newsletter</h4>
+            <p>Enterate de nuestras promociones mensuales, se de los primeros en enterarte de nuestras nuevas soluciones.</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit"  value="Suscribir">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>PbxHosting</strong> Todos los Derechos Reservados
+      </div>
+      <div class="credits">
+      </div>
+    </div>
+  </footer><!-- #footer -->
