04698560f8b9c8eebf11435f9e46bd7f8c4142e8
AldairCruz501 <acruz.pbxhosting@gmail.com>
2025-01-27 12:52:47 -0600

    Sitio Web Publismart

---
+++ publismart-Eder/web/enviar.php
@@ -0,0 +1,56 @@
+<?php
+
+if ($_POST['g-recaptcha-response'] == '') {
+
+} else {
+$obj = new stdClass();
+$obj->secret = "6Ld31_ciAAAAAJMEB7RqA1Q4p35Iowm5zC55SaZM";
+$obj->response = $_POST['g-recaptcha-response'];
+$obj->remoteip = $_SERVER['REMOTE_ADDR'];
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+
+$options = array(
+'http' => array(
+'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+'method' => 'POST',
+'content' => http_build_query($obj)
+)
+);
+$context = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+$validar = json_decode($result);
+
+/*  FIN DE CAPTCHA   */
+
+if ($validar->success) {
+$correo= trim($_POST['correo']);
+$nombre = trim($_POST['nombre']);
+$celular = trim($_POST['celular']);
+$mensaje = trim($_POST['mensaje']);
+$servicio = trim($_POST['servicio']);
+
+
+$consulta =  "Nombre: " . $nombre . "\n Correo: " . $correo .  "\n Numero de contacto: " . $celular  .  "\n Servicio de interes: " . $direccion  .  "\n Mensaje: " . $mensaje;
+          echo '
+                    <script>
+                         alert("Gracias por comunicarte");
+                         window.location = "https://grupopublismart.com.mx/";
+                    </script>        
+               ';
+
+          mail("echong.pbxhosting@gmail.com", "Formulario Publismart", $consulta);header("location:index.html");
+} 
+
+     else {
+          echo '
+          <script>
+               alert("Error resuelva el captcha");
+               window.location = "https://grupopublismart.com.mx/";
+          </script>        
+          ';
+          header("location:index.html");
+
+     }
+}
+?>
